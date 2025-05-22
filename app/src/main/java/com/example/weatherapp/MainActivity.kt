4d311607b8b17186353bdf9ce6b57ae04248109b
joaoTorpe <victotorpe10@gmail.com>
2025-05-22 10:48:33 -0300

    Pratica 2 - Parte 1: Criando as novas telas

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -1,24 +1,14 @@
 package com.example.weatherapp
 
-import android.annotation.SuppressLint
-import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.weatherapp.ui.HomePage
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -34,27 +24,5 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-@SuppressLint("ContextCastToActivity")
-@Composable
-fun HomePage(modifier: Modifier = Modifier){
-    val activity = LocalContext.current as? Activity
-    Column(
-        modifier = modifier.padding(16.dp).fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = CenterHorizontally
-    ) {
-        Text(
-            text = "Hello my friend",
-            modifier = modifier
-        )
 
-        Button(
-            onClick = {
-                activity?.finish()
-            }
-        ) {
-            Text("Sair")
-        }
-    }
-}
 
